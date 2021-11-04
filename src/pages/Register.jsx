104326e96a993cfd551b0330881010c33c33cd9a
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-03 23:28:18 -0300

    Add validation in register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,4 @@
-import React, { useContext, useState } from 'react';
-import AppContext from '../context/AppContext';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 function Register() {
@@ -8,6 +6,8 @@ function Register() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [clicked, setClicked] = useState(false);
   const history = useHistory();
 
   // function handleClick() {
@@ -17,46 +17,96 @@ function Register() {
   //   localStorage.setItem('user', JSON.stringify(localStorageEmail));
   // }
 
-  const verifyEmailAndPassword = () => {
+  const verifyName = () => {
+    const nameAndSurname = /[^\s]+\s+[^\s]+/;
+    const nameValid = nameAndSurname.test(name);
+    if (name === '') {
+      setError('O campo "nome" é obrigatório') 
+      return;
+    };
+    if(!nameValid) {
+      setError('O campo "nome" deve ser preenchido com nome e sobrenome');
+      return;
+    };
+  }
+
+  const verifyEmail = () => {
     const reg = /\S+@\S+\.\S+/
       .test(email);
-    const maxLenght = 6;
-    if (reg && password.length > maxLenght) {
-      return false;
+    if (email === '') {
+      setError('O campo "email" é obrigatório');
+      return;
+    };
+    if(!reg) {
+      setError('Formato inválido para o campo "email"');
+      return;
+    };
+  }
+
+  const verifyPassword = () => {
+    if(password === '') {
+      setError('O campo "senha" é obrigatório');
+      return;
+    };
+    if(password.length < 6){
+      setError('Senha deve conter no mínimo 6 caracteres');
+      return;
     }
-    return true;
-  };
+    if(password !== confirmPassword) {
+      setError('Senha inválida');
+      return;
+    };
+  }
+
+  useEffect(() => {
+    setClicked(false);
+    setError('');
+    verifyPassword();
+    verifyEmail();
+    verifyName();
+  }, [name, email, password, confirmPassword])
 
   const register = () => {
+    setClicked(true)
+    if(error === ''){
       history.push('/');
+      return;
+    }
   }
 
   return (
     <section>
+      <label htmlFor="name">Nome:</label>
       <input
+        id="name"
         type="text"
         placeholder="Nome"
         onChange={ (e) => setName(e.target.value) }
       />
+      <label htmlFor="email">Email:</label>
       <input
+        id="email"
         type="email"
         placeholder="E-mail"
         onChange={ (e) => setEmail(e.target.value) }
       />
+      <label htmlFor="password">Senha:</label>
       <input
+        id="password"
         type="password"
         placeholder="Senha"
         onChange={ (e) => setPassword(e.target.value) }
       />
+      <label htmlFor="confirmPassword">Confirmar senha:</label>
       <input
+        id="confirmPassword"
         type="password"
         placeholder="Confirmar senha"
         onChange={ (e) => setConfirmPassword(e.target.value) }
       />
+      { clicked && <div>{ error }</div> }
       <button
-        type="button"
-        data-testid="login-submit-btn"
-        disabled={ verifyEmailAndPassword() }
+        type="submit"
         onClick={ () => register() }
       >
         Cadastrar
