a66f0abaf5b285bbaa32f4f0fe7880a74882d52e
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-04 18:12:29 -0300

    Add private route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
-import { Switch , Route } from 'react-router-dom';
+import { Switch , Route, Redirect } from 'react-router-dom';
 import AppProvider from './context/AppProvider';
 import Login from './pages/Login';
 import Home from './pages/Home'
 import Register from './pages/Register'
+
+const isAutenticated = async () => {
+  const token = localStorage.getItem('token')
+  const requestOptions = {
+    method: 'GET',
+    headers: {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': `${token}`},
+  }
+  const isValid = await fetch('http://localhost:8080/validateToken', requestOptions);
+  const response = await isValid.json();
+  if(response.message && response.message === 'Authorized'){
+    return true
+  }
+  if(response.err && response.err.code === 400){
+    return false
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest}) => (
+  <Route {...rest} render={props => (
+    isAutenticated() ? (
+    <Component {...props} />
+    ) : (
+      <Redirect to={{ pathname: '/', state: { from: props.loctation }}} />
+    )
+  )} />
+)
 
 function App() {
   return (
     <AppProvider>
       <Switch>
         <Route exact path="/" component={ Login }/>
-        <Route exact path="/home" component={ Home }/>
+        <PrivateRoute path="/home" component={ Home }/>
         <Route exact path="/register" component={ Register }/>
       </Switch>
     </AppProvider>
